8ef3161636c032697034a1c92e5ac722063b8b36
Titusz003 <79099038+Titusz003@users.noreply.github.com>
2021-04-12 10:46:05 +0200

    Changes in draw-chess-table.ts

--- Week-1/day-3/draw-chess-table.ts
+++ Week-1/day-3/draw-chess-table.ts
@@ -2,14 +2,29 @@
 
 export{}
 
-let white: string="% % % % % % % % "
-let black: string=" % % % % % % % %"
+let black: string="";
 
 for(let i=1; i<=8;i++){
+    black="";
     if(i%2===0){
-        console.log(black);
+       for (let k=1;k<=16;k++){
+           if(k%2===0){
+               black= black+"%";
+           }
+           else{black=black+" ";
+        }
+    }
+    console.log(black); 
     }
     else{
-        console.log(white);
-    }
+    for(let k=1;k<=16;k++){
+        if(k%2===0){
+        black=black+" ";
+        }
+        else{
+            black=black+"%" 
+    }   
+}
+console.log(black);
+}
 }
